ef0b3599603a5588c6148c1967d2dcdf611f3fe7
Bongani Mashele <bjmashele@gmail.com>
2019-10-25 16:40:45 -0500

    Added the GET all_books route

--- server/app.py
+++ server/app.py
@@ -1,6 +1,31 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+# dummy data
+
+BOOKS = [
+    {
+        'title': 'Things Fall Apart',
+        'author': 'Chinua Achebe',
+        'started_reading': True
+    },
+    {
+        'title': 'Aminal Farm',
+        'author': 'George Orwell',
+        'started_reading': True
+    },
+    {
+        'title': 'Wizard of the Crow',
+        'author': 'Ngugi wa Thiong\'o',
+        'started_reading': True
+    },
+    {
+        'title': 'Poor Economics',
+        'author': 'Abhijit Banerjee',
+        'started_reading': False
+    }
+    
+]
 #config
 DEBUG = True
 
@@ -16,6 +41,13 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('pong!')
 
+# get all books
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
 
 if __name__== '__main__':
     app.run()
